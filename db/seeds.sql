260c63952c79f5228cd9cc8417229d2b69a100d6
Anne Wilson <annewilson2591@gmail.com>
2019-03-29 12:52:13 -0600

    file structure

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO burgers (burger, devoured) VALUES ('Bacon Cheese Burger', 'false');
+INSERT INTO burgers (burger, devoured) VALUES ('Chipotle Turkery Burger', 'false');
+INSERT INTO burgers (burger, devoured) VALUES ('Cheddar Jalapeno Chicken Burger', 'false');
